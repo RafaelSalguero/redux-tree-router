cb6d5611d3dd47ff9db3ccf02bf7a981a41a1cde
rafa <r.salguero@garsalabs.com>
2020-06-17 12:27:43 -0500

    use in route /location

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
-import { isLocationSubrouteOf } from "../../../logic/location";
+import { isLocationSubrouteOf, getLocation } from "./logic/location";
+import { ReadyRoute } from "./logic";
 
 /**Obtiene la ruta actual */
 function useCurrentLocation(): string {
@@ -7,11 +8,19 @@ function useCurrentLocation(): string {
 }
 
 /**
+ * Devuelve true si @param route es la ruta activa
+ */
+export function useInRoute(route: ReadyRoute, strict: boolean = false) {
+    const routeLocation = getLocation(route);
+    return useInLocation(routeLocation, strict);
+}
+
+/**
  * Devuelve true si la @param location es la ruta activa
  * @param location La ruta que se desea saber si es activa, es el type del action
  * @param strict False para devolver true si esta activa la ruta o sus hijos, true para solo la ruta
  */
-export function useRoute(location: string, strict: boolean = false) {
+export function useInLocation(location: string, strict: boolean = false) {
     const current = useCurrentLocation();
     if (location == current)
         return true;
--- src/ui/menu/view/index.ts
+++ src/ui/menu/view/index.ts
@@ -1,3 +1,3 @@
 export * from "./view";
 export {  RouteItemProps} from "./types";
-export * from "./hooks";
+export * from "../../../hooks";
