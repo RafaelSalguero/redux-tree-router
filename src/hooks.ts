fd1ffda60acb471c91ae5ba5d5e45fd6f606ee4d
rafa <r.salguero@garsalabs.com>
2020-09-24 11:07:25 -0500

    useRouteKey

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,31 @@
 import { useSelector } from "react-redux";
 import { isLocationSubrouteOf, getLocation } from "./logic/location";
-import { ReadyRoute } from "./logic";
+import { findRouteByLocation, isSubroute, listSubs, ReadyRoute } from "./logic";
+import { conf, getName, OmitConfSymbol, readyConf } from "./logic/types";
+import { firstMap, last } from "simple-pure-utils";
 
 /**Obtiene la ruta actual */
 function useCurrentLocation(): string {
     return useSelector((state: any) => state.location.type);
 }
 
+/**Dado un mapa de rutas, obtiene la clave de la ruta actual, encaja tambien las subrutas */
+export function useRouteKey<T extends ReadyRoute>(base: T): (keyof OmitConfSymbol<T>) | null {
+    const location = useCurrentLocation();
+    const currItem = findRouteByLocation(base, location);
+
+    if (currItem == null)
+        return null;
+
+    const subs = listSubs(base);
+    const key = firstMap(subs, x => isSubroute(x, currItem), x => getName(x)) as keyof OmitConfSymbol<T>;
+
+    return key;
+}
+
 /**
- * Devuelve true si @param route es la ruta activa
+ * Returns true if @param route is the active route
+ * @param strict True to only match @param route, false to match also subroutes
  */
 export function useInRoute(route: ReadyRoute, strict: boolean = false) {
     const routeLocation = getLocation(route);
@@ -16,9 +33,9 @@ export function useInRoute(route: ReadyRoute, strict: boolean = false) {
 }
 
 /**
- * Devuelve true si la @param location es la ruta activa
- * @param location La ruta que se desea saber si es activa, es el type del action
- * @param strict False para devolver true si esta activa la ruta o sus hijos, true para solo la ruta
+ * True if @param location is the active route
+ * @param location The action type of the tested route
+ * @param strict True to only match the tested route, false to match also subruoutes
  */
 export function useInLocation(location: string, strict: boolean = false) {
     const current = useCurrentLocation();
